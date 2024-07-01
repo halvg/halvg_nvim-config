0fce025ccd8743a8498adea849d27243c44827b6
Héctor Álvarez <hector@mail.com>
2024-07-01 08:54:21 +0200

    Changes to config

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -14,10 +14,10 @@ vim.keymap.set('i', 'jj', '<Esc>', { noremap = true })
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>en', vim.diagnostic.goto_next, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', '<leader>ep', vim.diagnostic.goto_prev, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>es', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>el', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -48,81 +48,94 @@ vim.keymap.set('n', '<A-1>', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' })
 --
 -- [[ Plugin specific Keymaps ]]
 -- Plugin specfic keymaps
+local M = {}
 
+-- --------------------------
 -- Conform (autoformatting) key mappings
+-- --------------------------
 local conformFormat = function()
   require('conform').format { async = true, lsp_fallback = true }
 end
 vim.keymap.set('n', '<leader>f', conformFormat, { desc = '[F]ormat buffer' })
+-- --------------------------
+-- --------------------------
 
-local M = {}
-
+-- --------------------------
 -- LSP-specific key mappings
-M.lsp_keymaps = function(bufnr)
-  local function map(keys, func, desc)
-    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = 'LSP: ' .. desc })
+-- --------------------------
+M.lsp_keymaps = function(bufnr, client)
+  local function map(mode, keys, func, desc)
+    vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = 'LSP: ' .. desc })
   end
 
-  -- Key mappings for LSP-related actions
-  map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-  map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-  map('<leader>gy', require('telescope.builtin').lsp_type_definitions, '[G]oto T[y]pe Definition')
-  map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  map('<leader>sy', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[S]earch Workspyce S[y]mbols')
-  map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-  map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-  map('K', vim.lsp.buf.hover, 'Hover Documentation')
+  local toggle_inlay_hints = function()
+    if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+      vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled(bufnr))
+    end
+  end
 
+  -- ---- Key mappings for LSP-related actions ----
+  -- Goto
+  map('n', 'gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+  map('n', 'gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  map('n', 'gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+  map('n', 'gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  map('n', '<leader>gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+  map('n', '<leader>gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  map('n', '<leader>gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+  map('n', '<leader>gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  map('n', '<leader>gt', require('telescope.builtin').lsp_type_definitions, '[G]oto T[y]pe Definition')
+  map('n', '<leader>gy', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  -- Refactors
+  map('n', '<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  -- Actions
+  map({ 'n', 'v' }, '<leader>li', vim.lsp.buf.code_action, 'Code Action')
+  map('n', 'K', vim.lsp.buf.hover, 'Hover Documentation')
   -- Additional mappings for inlay hints, if supported
-  if vim.lsp.inlay_hint then
-    map('<leader>th', function()
-      vim.lsp.inlay_hint(bufnr, nil, vim.lsp.inlay_hint.is_enabled(bufnr))
-    end, '[T]oggle Inlay [H]ints')
-  end
+  map('n', '<leader>33h', toggle_inlay_hints, '[T]oggle Inlay [H]ints')
 end
+-- --------------------------
+-- --------------------------
 
+-- --------------------------
 -- Telescope key mappings
+-- --------------------------
 M.telescope_keymaps = function()
-  -- See `:help telescope.builtin`
-  local builtin = require 'telescope.builtin'
-  vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-  vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-  vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-  vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-  vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-  vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-  vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-  vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-  vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-  vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+  -- See `:help telescope.builtins
+  local telescope_builtin = require 'telescope.builtin'
 
-  -- Slightly advanced example of overriding default behavior and theme
-  vim.keymap.set('n', '<leader>/', function()
-    -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-    builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+  local find_config_files = function()
+    telescope_builtin.find_files { cwd = vim.fn.stdpath 'config' }
+  end
+
+  local search_text_this_buffer = function()
+    telescope_builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
       winblend = 10,
       previewer = false,
     })
-  end, { desc = '[/] Fuzzily search in current buffer' })
+  end
 
-  -- It's also possible to pass additional configuration options.
-  --  See `:help telescope.builtin.live_grep()` for information about particular keys
-  vim.keymap.set('n', '<leader>s/', function()
-    builtin.live_grep {
-      grep_open_files = true,
-      prompt_title = 'Live Grep in Open Files',
-    }
-  end, { desc = '[S]earch [/] in Open Files' })
-
-  -- Shortcut for searching your Neovim configuration files
-  vim.keymap.set('n', '<leader>sn', function()
-    builtin.find_files { cwd = vim.fn.stdpath 'config' }
-  end, { desc = '[S]earch [N]eovim files' })
+  vim.keymap.set('n', '<leader>ss', telescope_builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+  vim.keymap.set('n', '<leader>sf', telescope_builtin.find_files, { desc = '[S]earch [F]iles' })
+  vim.keymap.set('n', '<leader>sy', telescope_builtin.lsp_dynamic_workspace_symbols, { desc = '[S]earch Workspace S[y]mbols' })
+  vim.keymap.set('n', '<leader>sg', telescope_builtin.live_grep, { desc = '[S]earch by [G]rep' })
+  vim.keymap.set('n', '<leader>sif', telescope_builtin.current_buffer_fuzzy_find, { desc = '[S]earch [i]n [F]ile' })
+  vim.keymap.set('n', '<leader>sh', telescope_builtin.help_tags, { desc = '[S]earch [H]elp' })
+  vim.keymap.set('n', '<leader>sk', telescope_builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+  vim.keymap.set('n', '<leader>sw', telescope_builtin.grep_string, { desc = '[S]earch current [W]ord' })
+  vim.keymap.set('n', '<leader>sd', telescope_builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+  vim.keymap.set('n', '<leader>se', telescope_builtin.resume, { desc = '[S]earch R[e]sume' })
+  vim.keymap.set('n', '<leader>sr', telescope_builtin.oldfiles, { desc = '[S]earch [R]ecent Files' })
+  vim.keymap.set('n', '<leader>s,', find_config_files, { desc = '[S]earch [N]eovim files' })
+  vim.keymap.set('n', '<leader>/', search_text_this_buffer, { desc = '[/] Fuzzily search in current buffer' })
+  vim.keymap.set('n', '<leader>bb', telescope_builtin.buffers, { desc = '[ ] Find existing buffers' })
 end
+-- --------------------------
+-- --------------------------
 
+-- --------------------------
 -- Mini surround operations
+-- --------------------------
 M.mini_surround_keymaps = {
   mappings = {
     add = '<leader>asa', -- Add surrounding in Normal and Visual modes
@@ -137,5 +150,7 @@ M.mini_surround_keymaps = {
     suffix_next = 'n', -- Suffix to search with "next" method
   },
 }
+-- --------------------------
+-- --------------------------
 
 return M
--- lua/user/kickstart/plugins/lspconfig.lua
+++ lua/user/kickstart/plugins/lspconfig.lua
@@ -48,15 +48,15 @@ return {
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
 
-          require('user.keymaps').lsp_keymaps(event.buf)
+          require('user.keymaps').lsp_keymaps(event.buf, client)
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
           --
           -- When you move your cursor, the highlights will be cleared (the second autocommand).
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
           if client and client.server_capabilities.documentHighlightProvider then
             local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
@@ -79,7 +79,6 @@ return {
               end,
             })
           end
-
         end,
       })
 
